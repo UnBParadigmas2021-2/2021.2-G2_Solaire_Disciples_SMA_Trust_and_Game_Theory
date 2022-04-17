c997cf4d0a9b33b2d44e06decdc34046796221a2
Cainã <caina20000@gmail.com>
2022-04-17 01:46:22 -0300

    adiciona agentes basicos e tentativa de matchmaking

--- src/agent_example_1.py
+++ src/agent_example_1.py
@@ -6,23 +6,62 @@ from pade.misc.utility import display_message, start_loop
 from pade.core.agent import Agent
 from pade.acl.aid import AID
 from sys import argv
+from itertools import groupby
+from itertools import combinations
 
-class AgenteHelloWorld(Agent):
+
+class Machine(Agent):
     def __init__(self, aid):
-        super(AgenteHelloWorld, self).__init__(aid=aid)
-        display_message(self.aid.localname, 'Hello World!')
+        super(Machine, self).__init__(aid=aid)
+        display_message(self.aid.localname, 'BEEP BOOP! Im the MACHINE!')
+
+class Cooperator(Agent):
+    has_been_cheated_on = False
+
+    def __init__(self, aid):
+        super(Cooperator, self).__init__(aid=aid)
+        display_message(self.aid.localname, 'Hello World! Im a COOPERATOR! I ALWAYS COOPERATE')
+
+
+class Cheater(Agent):
+    def __init__(self, aid):
+        super(Cheater, self).__init__(aid=aid)
+        display_message(self.aid.localname, 'Hello World! Im a CHEATER! I ALWAYS CHEAT')
 
 
 if __name__ == '__main__':
-
-    agents_per_process = 5
+    cheater = 3
+    cooperator = 7
     c = 0
     agents = list()
-    for i in range(agents_per_process):
+    player_names = list ()
+    ## intializes the machine
+    port = int(argv[1])
+    c += 1000
+    machine_name = 'machine_{}@localhost:{}'.format(port, port)
+    machine_init = Machine(AID(name=machine_name))
+    agents.append(machine_init)
+
+    ## initializes copycats
+    for i in range(cooperator):
         port = int(argv[1]) + c
-        agent_name = 'agent_hello_{}@localhost:{}'.format(port, port)
-        agente_hello = AgenteHelloWorld(AID(name=agent_name))
-        agents.append(agente_hello)
+        cooperator_name = 'copycat_{}@localhost:{}'.format(port, port)
+        cooperator_init = Cooperator(AID(name=cooperator_name))
+        player_names.append(cooperator_name)
+        agents.append(cooperator_init)
         c += 1000
-    
+
+    ## initializes cheater
+    for i in range(cheater):
+        port = int(argv[1]) + c
+        cheater_name = 'cheater_{}@localhost:{}'.format(port, port)
+        cheater_init = Cheater(AID(name=cheater_name))
+        player_names.append(cheater_name)
+        agents.append(cheater_init)
+        c += 1000
+
+    ## creates matchmaking
+    matchmaking = list(combinations(player_names, 2))
+
+
     start_loop(agents)
