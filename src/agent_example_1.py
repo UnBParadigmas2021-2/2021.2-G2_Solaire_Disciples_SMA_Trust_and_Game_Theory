d7850c8a79f711892597673d1551964f4f5174fa
ErickGiffoni <giffoni.erick@gmail.com>
2022-04-18 10:21:43 -0300

    refactor: usa nova funcao para inicializar os agentes

--- src/agent_example_1.py
+++ src/agent_example_1.py
@@ -7,7 +7,7 @@ from pade.acl.messages import ACLMessage
 from pprint import pprint
 from pade.acl.aid import AID
 from sys import argv
-from itertools import groupby
+from itertools import count, groupby
 from itertools import combinations
 
 
@@ -118,6 +118,8 @@ class PlayerProtocol(FipaSubscribeProtocol):
             reply.set_content(False)
         elif(self.persona == 'Cheater'):
             reply.set_content(True)
+        # TO-DO: elif is Copycat
+        # TO-DO: elif is Grudger
         reply.set_performative(ACLMessage.INFORM)
         self.agent.send(reply)
 
@@ -190,23 +192,12 @@ class Grudger(Agent):
                         'Hi! Im a Grudger: I will cooperate... unless my \
                         adversary cheats on me (then I will always cheat)!')
 
-if __name__ == '__main__':
-    cheater     = 3
-    cooperator  = 7
-    copycat     = 3
-    grudger     = 4
-    c = 0
-    agents = list()
+def init_agents(agents: list, number_of_agents, port):
     player_names = list()
+    c = 1
 
-    port = int(argv[1]) + c
-    machine_name = 'machine_{}@localhost:{}'.format(port, port)
-    machine_instance = AgentMachine(AID(name=machine_name))
-    agents.append(machine_instance)
-    c += 1
-
-    for i in range(cooperator):
-        port = int(argv[1]) + c
+    for i in range(number_of_agents[0]):
+        port = port + c
         cooperator_name = 'cooperator_{}@localhost:{}'.format(port, port)
         cooperator_init = AgentPlayer(AID(name=cooperator_name),
                                       machine=machine_instance,
@@ -215,8 +206,8 @@ if __name__ == '__main__':
         agents.append(cooperator_init)
         c += 1
 
-    for i in range(cheater):
-        port = int(argv[1]) + c
+    for i in range(number_of_agents[1]):
+        port = port + c
         cheater_name = 'cheater_{}@localhost:{}'.format(port, port)
         cheater_init = AgentPlayer(AID(name=cheater_name),
                                    machine=machine_instance,
@@ -225,8 +216,8 @@ if __name__ == '__main__':
         agents.append(cheater_init)
         c += 1
 
-    for i in range(copycat):
-        port = int(argv[1]) + c
+    for i in range(number_of_agents[2]):
+        port = port + c
         copycat_name = 'copycat_{}@localhost:{}'.format(port, port)
         copycat_init = AgentPlayer(AID(name=copycat_name),
                                    machine=machine_instance,
@@ -235,8 +226,8 @@ if __name__ == '__main__':
         agents.append(copycat_init)
         c += 1
         
-    for i in range(grudger):
-        port = int(argv[1]) + c
+    for i in range(number_of_agents[3]):
+        port = port + c
         grudger_name = 'grudger_{}@localhost:{}'.format(port, port)
         grudger_init = AgentPlayer(AID(name=grudger_name),
                                    machine=machine_instance,
@@ -245,4 +236,19 @@ if __name__ == '__main__':
         agents.append(grudger_init)
         c += 1
 
+    return
+
+if __name__ == '__main__':
+    cheater     = 3
+    cooperator  = 7
+    copycat     = 3
+    grudger     = 4
+    agents = list()
+
+    port = int(argv[1])
+    machine_name = 'machine_{}@localhost:{}'.format(port, port)
+    machine_instance = AgentMachine(AID(name=machine_name))
+    agents.append(machine_instance)
+
+    init_agents(agents=agents, number_of_agents=[cheater, cooperator, copycat, grudger], port=port)
     start_loop(agents)
