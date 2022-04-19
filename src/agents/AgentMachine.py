b98a2dcbf6a649586e679eb114934326aac0ff23
Cainã <caina20000@gmail.com>
2022-04-18 21:41:16 -0300

    fix numerous bugs

--- src/agents/AgentMachine.py
+++ src/agents/AgentMachine.py
@@ -3,56 +3,70 @@ from pade.core.agent import Agent
 from pade.misc.utility import display_message
 from pade.acl.filters import Filter as f
 from pade.acl.messages import ACLMessage
-
+from pade.behaviours.protocols import FipaSubscribeProtocol
 from utils.DummyTime import DummyTime
-from utils.MachineProtocol import MachineProtocol
 
 
 class AgentMachine(Agent):
-   play_list = list()
+    play_list = list()
 
-   def __init__(self, aid):
-      super(AgentMachine, self).__init__(aid=aid)
-      display_message(self.aid.localname, 'BEEP BOOP! Im the MACHINE!')
-      self.machine_subscription_protocol = MachineProtocol(self)
-      self.dummy_timed_message = DummyTime(self,
-                                          self.machine_subscription_protocol.notify)
+    def __init__(self, aid):
+        super(AgentMachine, self).__init__(aid=aid)
+        display_message(self.aid.localname, 'BEEP BOOP! Im the MACHINE!')
+        self.machine_subscription_protocol = MachineProtocol(self)
+        self.dummy_timed_message = DummyTime(self,
+                                             self.machine_subscription_protocol.notify)
 
-      self.behaviours.append(self.machine_subscription_protocol)
-      self.behaviours.append(self.dummy_timed_message)
+        self.behaviours.append(self.machine_subscription_protocol)
+        self.behaviours.append(self.dummy_timed_message)
 
-   def react(self, message):
-      super(AgentMachine, self).react(message)
-      filter = f()
-      filter.performative = ACLMessage.REQUEST
-      if filter.filter(message=message):
-         reply = message.create_reply()
-         reply.set_performative(ACLMessage.AGREE)
-         reply.add_receiver(message.sender)
-         self.send(reply)
-      else:
-         if(isinstance(message.content, bool)):
-               self.play_list.append({'play': message.content, 'player': message.sender.name})
-      if len(self.play_list) == 2:
-         print('{} vs {}'.format(self.play_list[0]['player'], self.play_list[1]['player']))
-         if self.play_list[0]['play'] == False and self.play_list[1]['play'] == False:
-               print('Ninguem roubou!')
-         elif self.play_list[0]['play'] == True and self.play_list[1]['play'] == False:
-               print('{} roubou!'.format(self.play_list[0]['player']))
-         elif self.play_list[0]['play'] == False and self.play_list[1]['play'] == True:
-               print('{} roubou!'.format(self.play_list[1]['player']))
-         elif self.play_list[0]['play'] == True and self.play_list[1]['play'] == True:
-               print('Os dois roubaram!')
-         self.play_list= []
-      # display_message(self.aid.localname, message)
-      pass
+    def react(self, message):
+        super(AgentMachine, self).react(message)
+        filter = f()
+        filter.performative = ACLMessage.REQUEST
+        if filter.filter(message=message):
+            reply = message.create_reply()
+            reply.set_performative(ACLMessage.AGREE)
+            reply.add_receiver(message.sender)
+            self.send(reply)
+        else:
+            if(isinstance(message.content, bool)):
+                if(len(self.play_list) == 0):
+                    print('-------------------')
+                self.play_list.append(
+                    {'play': message.content, 'player': message.sender.name})
+                print('{} fez sua jogada!'.format(message.sender.name))
+        if len(self.play_list) == 2:
+            print('{} vs {}'.format(
+                self.play_list[0]['player'], self.play_list[1]['player']))
+            if self.play_list[0]['play'] == False and self.play_list[1]['play'] == False:
+                print('Ninguem roubou!')
+            elif self.play_list[0]['play'] == True and self.play_list[1]['play'] == False:
+                print('{} roubou!'.format(self.play_list[0]['player']))
+            elif self.play_list[0]['play'] == False and self.play_list[1]['play'] == True:
+                print('{} roubou!'.format(self.play_list[1]['player']))
+            elif self.play_list[0]['play'] == True and self.play_list[1]['play'] == True:
+                print('Os dois roubaram!')
+            self.play_list = []
+        pass
 
-   # def on_start(self):
-   #     message = ACLMessage(ACLMessage.REQUEST)
-   #     # message.set_performative()
-   #     # message.add_receiver(AID(self.match[0]))
-   #     # message.add_receiver(AID(self.match[1]))
-   #     message.set_content(
-   #         'Jogador {} e {}, Façam suas escolhas!'.format(str(match[0]),
-   #                                                        str(match[1])))
-   #     self.notify(message)
+
+class MachineProtocol(FipaSubscribeProtocol):
+    def __init__(self, agent):
+        super(MachineProtocol, self).__init__(agent,
+                                              message=None,
+                                              is_initiator=False)
+
+    def handle_subscribe(self, message):
+        self.register(message.sender)
+        resposta = message.create_reply()
+        resposta.set_performative(ACLMessage.AGREE)
+        self.agent.send(resposta)
+
+    def handle_cancel(self, message):
+        self.deregister(self, message.sender)
+        display_message(self.agent.aid.name, message.content)
+
+    def notify(self, message):
+        display_message(self.agent.aid.name, '')
+        super(MachineProtocol, self).notify(message)
--- src/utils/DummyTime.py
+++ src/utils/DummyTime.py
@@ -4,14 +4,14 @@ from pade.acl.messages import ACLMessage
 
 class DummyTime(TimedBehaviour):
 
-   def __init__(self, agent, notify):
-      super(DummyTime, self).__init__(agent, 1)
-      self.notify = notify
-      self.inc = 0
+    def __init__(self, agent, notify):
+        super(DummyTime, self).__init__(agent, 1)
+        self.notify = notify
+        self.inc = 0
 
-   def on_time(self):
-      super(DummyTime, self).on_time()
-      message = ACLMessage(ACLMessage.INFORM)
-      message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
-      self.notify(message)
-      self.inc += 0.1
+    def on_time(self):
+        super(DummyTime, self).on_time()
+        message = ACLMessage(ACLMessage.INFORM)
+        message.set_protocol(ACLMessage.FIPA_SUBSCRIBE_PROTOCOL)
+        self.notify(message)
+        self.inc += 0.1
